b05a6a71f7be24afff7dc9a3d75893c9bd933a8a
Matt Martin <mjm2312@columbia.edu>
2021-04-19 10:05:51 -0400

    Changed host/port args for EC2

--- csv/copy.sh
+++ csv/copy.sh
@@ -18,12 +18,20 @@
 #cat answers_transformed.csv | psql -h ec2-18-220-103-44.us-east-2.compute.amazonaws.com -U postgres -d matttest -c "COPY sdc.answers FROM STDIN WITH DELIMITER ',' CSV HEADER";
 
 #in K8s
+# echo "copying answers_photos..."
+# cat answers_photos_transformed.csv | psql -h 192.168.64.2 -U postgres -p 30224 -d matttest -c "COPY sdc.answers_photos FROM STDIN WITH DELIMITER ',' CSV HEADER";
+# echo "copying questions..."
+# cat questions_transformed.csv | psql -h 192.168.64.2 -p 30224 -U postgres -d matttest -c "COPY sdc.questions FROM STDIN WITH DELIMITER ',' CSV HEADER";
+# echo "copying answers..."
+# cat answers_transformed.csv | psql -h 192.168.64.2 -p 30224 -U postgres -d matttest -c "COPY sdc.answers FROM STDIN WITH DELIMITER ',' CSV HEADER";
+
+#in K8s on aws
 echo "copying answers_photos..."
-cat answers_photos_transformed.csv | psql -h 192.168.64.2 -U postgres -p 30224 -d matttest -c "COPY sdc.answers_photos FROM STDIN WITH DELIMITER ',' CSV HEADER";
+cat answers_photos_transformed.csv | psql -h 54.202.54.201 -p 32706 -U postgres -d postgres -c "COPY sdc.answers_photos FROM STDIN WITH DELIMITER ',' CSV HEADER";
 echo "copying questions..."
-cat questions_transformed.csv | psql -h 192.168.64.2 -p 30224 -U postgres -d matttest -c "COPY sdc.questions FROM STDIN WITH DELIMITER ',' CSV HEADER";
+cat questions_transformed.csv | psql -h 54.202.54.201 -p 32706 -U postgres -d postgres -c "COPY sdc.questions FROM STDIN WITH DELIMITER ',' CSV HEADER";
 echo "copying answers..."
-cat answers_transformed.csv | psql -h 192.168.64.2 -p 30224 -U postgres -d matttest -c "COPY sdc.answers FROM STDIN WITH DELIMITER ',' CSV HEADER";
+cat answers_transformed.csv | psql -h 54.202.54.201 -p 32706 -U postgres -d postgres -c "COPY sdc.answers FROM STDIN WITH DELIMITER ',' CSV HEADER";
 
 
 
