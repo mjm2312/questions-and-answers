1297fa162a6a449f7c564227cd334c802b1a007b
Matt Martin <mjm2312@columbia.edu>
2021-04-23 12:22:56 -0400

    Updated HTTP response codes, corrected a few cases that left client hanging

--- src/db/models.js
+++ src/db/models.js
@@ -1,5 +1,4 @@
-const pool = require ('./');
-//console.log('what is pool in models.js ? ', pool)
+const pool = require('./');
 
 module.exports = { 
   //by default setting helpfulness, reported to 0.
@@ -25,33 +24,28 @@ module.exports = {
     function(err, res) {     
         if(err) {
             console.error('error running query', err);
-            res.sendStatus(500);
+            response.sendStatus(500);
             return;
           }
-        response.send('post ok')
-    })
-    
+        response.status(201).send('post ok')
+    })  
   },
 
   addAnswer: function(questionId, body, name, email, photos, response) {
     pool.query(
-      `(SELECT
-       MAX(id)+1 FROM sdc.answers)`,
-      
+      `(SELECT MAX(id)+1 
+      FROM sdc.answers)`,  
       function(err, res) {
         if(err) {
-          console.error('error running query', err);
-          res.status(500);
+          console.error('error finding id for new answer', err);
+          response.sendStatus(500);
           return;
         } 
-        //storeAnswerID in event of inserting mutiple photso
-        console.log('answerId', res.rows[0]['?column?'])
-        var answerID = Number(res.rows[0]['?column?']);
-
-        //reported, helpful 0 by default
-      
+        //storeAnswerID in event of inserting mutiple photos
+        const answerID = Number(res.rows[0]['?column?']);
+        //reported, helpful are 0 by default      
         pool.query(
-          `INSERT INTO sdc.answers 
+          `INSERT INTO sdc.answers
           (id, 
           question_id, 
           body, 
@@ -69,54 +63,41 @@ module.exports = {
           '${email}', 
           0, 
           0);`,
-          
           function(err, res) {
             if(err) {
-              console.error('error running query', err);
-              res.status(500);
+              console.error('error inserting answer', err);
+              response.sendStatus(500);
               return;
             } 
             
-            //console.log('passed the first insert')
             //only want to execute if there's atleast one photo
             if (photos.length > 0) {
-              var makeTemplate = (photoURL) => (`
+              let makeTemplate = (photoURL) => (`
               INSERT INTO sdc.answers_photos (id, answer_id, photo_url)
               VALUES ((SELECT MAX(id) FROM sdc.answers_photos)+1, 
               ${answerID}, 
               '${photoURL}');`);
 
               //make an array of queries;
-              var insertStatements = photos.map((photo)=> (makeTemplate(photo)));
-              var insertStatement = insertStatements.join('');
+              let insertStatements = photos.map((photo)=> (makeTemplate(photo)));
+              const insertStatement = insertStatements.join('');
               pool.query(insertStatement, function(err, res) {
                 if(err) {
                   console.error('error running query', err);
-                  res.status(500);
+                  response.sendStatus(500);
                   return;
                 } 
-                console.log('res after inserting', res);
+
+                response.status(201).send('post ok'); 
               })
             }
+            //posted answer without photos
+            response.status(201).send('post ok');
           })
-          response.send('post ok')
         })
   },
-  listAnswers: function(questionId, response) {
-    // var template = `	SELECT
-    // a.id answer_id,
-    // a.body body, 
-    // a.date_written date,
-    // a.answerer_name answerer_name,
-    // a.helpful helpfulness,
-    // ap.id ap_photo_id, 
-    // ap.answer_id ap_answer_id, 
-    // ap.photo_url ap_photo_url
-    // FROM sdc.answers a
-	  // LEFT OUTER JOIN sdc.answers_photos ap on a.id = ap.answer_id
-    // WHERE a.question_id = ${questionId} AND a.reported IN (0)`
-    //console.log(questionId, template);
 
+  listAnswers: function(questionId, response) {
     pool.query(
     `	SELECT
     a.id answer_id,
@@ -134,11 +115,11 @@ module.exports = {
     function(err, res) {
       if(err) {
         console.error('error running query', err);
-        res.status(500);
+        response.sendStatus(500);
         return;
       }
 
-      obj = {};
+      let obj = {};
 
       res.rows.forEach((x) => {
 
@@ -153,41 +134,38 @@ module.exports = {
           }
 
           if (x.ap_photo_id !== null && 
-            x.ap_answer_id !== null &&
-            x.ap_photo_url !== null) {  
-              obj[x.answer_id].photos.push({
-                id: x.ap_photo_id, 
-                url: x.ap_photo_url
-              })
-            }
+          x.ap_answer_id !== null &&
+          x.ap_photo_url !== null) {  
+            obj[x.answer_id].photos.push({
+            id: x.ap_photo_id, 
+            url: x.ap_photo_url
+            })
+          }
         } else { //will only see a repeat answer in this case because there are photos;
           if (x.ap_photo_id !== null && 
-            x.ap_answer_id !== null &&
-            x.ap_photo_url !== null) {  
-              obj[x.answer_id].photos.push({
-                id: x.ap_photo_id, 
-                url: x.ap_photo_url
-              })
-            }
+          x.ap_answer_id !== null &&
+          x.ap_photo_url !== null) {  
+            obj[x.answer_id].photos.push({
+              id: x.ap_photo_id, 
+              url: x.ap_photo_url
+            })
+          }
         }
-
       })
-      //console.log('the obj', obj);
-      var resultsArr = [];
-      var resultObj = {};
 
-      for (var i in obj) {
+      let resultsArr = [];
+      let resultObj = {};
+
+      for (let i in obj) {
         resultsArr.push(obj[i]);
       }
-      //don't forget about parge, count query params
+
       resultObj['question'] = questionId;
       resultObj['results'] = resultsArr;
+      response.send(resultObj); 
+    })
+  }, 
 
-      console.log('this is result Obj', resultObj)
-      response.send(resultObj);
-
-  })
-  }, //
   listQuestions: function(productId, response) {
     pool.query(`SELECT 
     q.product_id product_id,
@@ -214,16 +192,15 @@ module.exports = {
     function(err, res) {
       if(err) {
         console.error('error running query', err);
-        res.status(500);
+        response.sendStatus(500);
         return;
       }
     
-    obj = {};
-
-    res.rows.forEach(x => {
-      //another answer for a particular question
-      //when there
-      if (obj[x.question_id] !== undefined && 
+      let obj = {};
+      res.rows.forEach(x => {
+        //another answer for a particular question
+        //when there
+        if (obj[x.question_id] !== undefined && 
         obj[x.question_id].answers[x.answer_id] === undefined ) {  
         obj[x.question_id].answers[x.answer_id] = {
           id: x.answer_id,
@@ -232,24 +209,49 @@ module.exports = {
           answerer_name: x.answerer_name,
           helpfulness: x.helpfulness,
           photos: []
-          //photos
-       } //deal w photos later    
-
-       if (x.ap_photo_id !== null && 
-        x.ap_answer_id !== null &&
-        x.ap_photo_url !== null) {  
-          obj[x.question_id].answers[x.answer_id].photos.push({
-            id: x.ap_photo_id, 
-            url: x.ap_photo_url
-          })
-        }
-      
-      //if theres already a question and answer that exists, 
-      //a second answer with same id indicates a photo
-      } else if (obj[x.question_id] !== undefined && 
-        obj[x.question_id].answers[x.answer_id] !== undefined ) { 
+        }  
 
           if (x.ap_photo_id !== null && 
+          x.ap_answer_id !== null &&
+          x.ap_photo_url !== null) {  
+            obj[x.question_id].answers[x.answer_id].photos.push({
+              id: x.ap_photo_id, 
+              url: x.ap_photo_url
+            })
+          }
+        
+        //if theres already a question and answer that exists, 
+        //a second answer with same id indicates a photo
+        } else if (obj[x.question_id] !== undefined && 
+        obj[x.question_id].answers[x.answer_id] !== undefined ) { 
+          if (x.ap_photo_id !== null && 
+          x.ap_answer_id !== null &&
+          x.ap_photo_url !== null) {  
+            obj[x.question_id].answers[x.answer_id].photos.push({
+              id: x.ap_photo_id, 
+              url: x.ap_photo_url
+            })
+          }
+        //new question and new answer
+        } else {
+            obj[x.question_id] = {
+              question_id: x.question_id,
+              question_body: x.question_body,
+              question_date: x.question_date,
+              asker_name: x.asker_name,
+              question_helpfulness: x.question_helpfulness,
+              reported: x.reported,
+              answers : {}
+            }
+            obj[x.question_id].answers[x.answer_id] = {
+                id: x.answer_id,
+                body: x.body,
+                date: x.date,
+                answerer_name: x.answerer_name,
+                helpfulness: x.helpfulness,
+                photos: []
+            }
+            if (x.ap_photo_id !== null && 
             x.ap_answer_id !== null &&
             x.ap_photo_url !== null) {  
               obj[x.question_id].answers[x.answer_id].photos.push({
@@ -257,53 +259,19 @@ module.exports = {
                 url: x.ap_photo_url
               })
             }
-      //new question and new answer
-      } else {
-
-          obj[x.question_id] = {
-            question_id: x.question_id,
-            question_body: x.question_body,
-            question_date: x.question_date,
-            asker_name: x.asker_name,
-            question_helpfulness: x.question_helpfulness,
-            reported: x.reported,
-            answers : {}
-          }
-
-          obj[x.question_id].answers[x.answer_id] = {
-              id: x.answer_id,
-              body: x.body,
-              date: x.date,
-              answerer_name: x.answerer_name,
-              helpfulness: x.helpfulness,
-              photos: []
-            }
-          
-            if (x.ap_photo_id !== null && 
-                x.ap_answer_id !== null &&
-                x.ap_photo_url !== null) {  
-                  obj[x.question_id].answers[x.answer_id].photos.push({
-                    id: x.ap_photo_id, 
-                    url: x.ap_photo_url
-                  })
-                }
+        }
+      })
+      let resultsValues = [];
+      for (let i in obj) {
+        resultsValues.push(obj[i]);
       }
 
-    })
-    var resultsValues = [];
-    for (var i in obj) {
-      resultsValues.push(obj[i]);
+      let resultObj = {
+        product_id : productId,
+        results: resultsValues
+      }
+
+      response.send(resultObj);
+      })
     }
-
-    var resultObj = {
-      product_id : productId,
-      results: resultsValues
-    }
-
-    console.log('resultObj', resultObj)
-    response.send(resultObj);
-
-
-    })
-  }
 }
